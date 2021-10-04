7b7219d05ed7d2fd6c71f717a8bd2c79810fbdae
Gabriel Vargas <gabrielfvf@outlook.com>
2021-10-04 14:08:00 -0300

    WIP: Básico do lab5 funcionando

--- Laboratorio_5/silv_cli.py
+++ Laboratorio_5/silv_cli.py
@@ -2,23 +2,28 @@
 
 # Cliente de echo usando RPC
 import rpyc #modulo que oferece suporte a abstracao de RPC
+from select import select
+import sys
 
 # endereco do servidor de echo
 SERVIDOR = 'localhost'
 PORTA = 10001
 
+entradas = [sys.stdin]
+
 class cliente(rpyc.Service):
 	# executa quando uma conexao eh criada
-    def on_connect(self, conn):
-        print("Conexao iniciada:")
-    def teste(self, msg):
-        print(msg)
+	def on_connect(self, conn):
+		print("Conexao iniciada:")
+	def teste(self, msg):
+		print(msg)
 
 
 def iniciaConexao():
 	'''Conecta-se ao servidor.
 	Saida: retorna a conexao criada.'''
-	conn = rpyc.connect(SERVIDOR, PORTA, config={"allow_all_attrs": True}, service = cliente) 
+	conn = rpyc.connect(SERVIDOR, PORTA, config={"allow_all_attrs": True},service = cliente)
+	entradas.append(conn)
 	
 	print(type(conn.root)) # mostra que conn.root eh um stub de cliente
 	print(conn.root.get_service_name()) # exibe o nome da classe (servico) oferecido
@@ -29,25 +34,40 @@ def fazRequisicoes(conn):
 	'''Faz requisicoes ao servidor e exibe o resultado.
 	Entrada: conexao estabelecida com o servidor'''
 	# le as mensagens do usuario ate ele digitar 'fim'
-	while True: 
-		msg = input("Digite uma mensagem ('fim' para terminar):")
-		if msg == 'fim': break 
-
-		# envia a mensagem do usuario para o servidor
-		ret = conn.root.exposed_echo(msg)
-
-		# imprime a mensagem recebida
-		print(ret)
+	print("Digite uma mensagem ('fim' para terminar):")
+	while True:
+		leitura, _, _ = select(entradas, [], [])
+		for pronto in leitura:
+			if pronto == sys.stdin:
+				msg = input()
+				if msg == 'fim':
+					break
+				if msg == 'inscreve':
+					topico = input("Digite o topico para se inscrever: ")
+					teste = conn.root.inscreve(topico, myPrint)
+					print(teste)
+				# envia a mensagem do usuario para o servidor
+				elif msg == 'envia':
+					topico = input("Digite o topico para publicar o anúncio: ")
+					msg = input('Digite o conteúdo do anúncio: ')
+					ret = conn.root.echo(msg, topico)
+					print(ret)
+					# imprime a mensagem recebida
 
 	# encerra a conexao
 	conn.close()
+
+def myPrint(message):
+	print(message)
 
 def main():
 	'''Funcao principal do cliente'''
 	#inicia o cliente
 	conn = iniciaConexao()
+	bg = rpyc.BgServingThread(conn)
 	#interage com o servidor ate encerrar
 	fazRequisicoes(conn)
+	bg.stop()
 
 # executa o cliente
 if __name__ == "__main__":
--- Laboratorio_5/silv_serv.py
+++ Laboratorio_5/silv_serv.py
@@ -2,13 +2,14 @@
 
 # Servidor de echo usando RPC 
 import rpyc #modulo que oferece suporte a abstracao de RPC
-
 #servidor que dispara um processo filho a cada conexao
 from rpyc.utils.server import ThreadedServer 
 
+from threading import RLock
 # porta de escuta do servidor de echo
 PORTA = 10001
 
+broadcast_lock = RLock()
 lista_topicos = {}
 
 # classe que implementa o servico de echo
@@ -17,20 +18,43 @@ class Echo(rpyc.Service):
     conexao = ''
     def on_connect(self, conn):
         self.conexao = conn
-        print(conn)
-        print("Conexao iniciada:")
+        print("Conexao iniciada: ")
     def on_disconnect(self, conn):
-        print("Conexao finalizada:")
-    def exposed_echo(self, msg):
-        self.conexao.root.teste("Mensagem")
-        return msg
+        global lista_topicos
+        for topico in lista_topicos:
+            for connection in topico:
+                if conn == connection:
+                    topico.remove(conn)
+        print("Conexao finalizada: ")
+    def exposed_echo(self, msg, topico):
+        ret = self.envia_mensagem(msg, topico)
+        return ret
+        
+    def exposed_inscreve(self, topico, myPrint):
+        global lista_topicos
+        if topico in lista_topicos:
+            lista_topicos[topico].append(myPrint)
+        else:
+            lista_topicos[topico] = [myPrint]
+        return 'Inscrito com sucesso'
+    
+    def envia_mensagem(self, msg, topico):
+        global lista_topicos
+        with broadcast_lock:
+            if topico in lista_topicos:
+                if len(lista_topicos[topico]) > 0:
+                    for myPrint in lista_topicos[topico]:
+                        myPrint(msg)
+                    return 'Envio feito com sucesso!'
+                else:
+                    print('entrou no else')
+                    return 'Não há clientes inscritos neste tópico!'
+            else:
+                lista_topicos[topico] = []
+                return 'Este tópico não existia e foi criado!'
   
-def envia_mensagem(msg, topico):
-    for conn in lista_topicos[topico]:
-        conn.root.mostrarMsg(msg)
 
-def inscreve(topico, conn):
-    lista_topicos[topico].append(conn)
+
 
 def processa_msg(msg):
     jsonTratado = json.loads(msg)
@@ -40,7 +64,7 @@ def processa_msg(msg):
 
 # dispara o servidor
 if __name__ == "__main__":
-	srv = ThreadedServer(Echo, port = PORTA)
+	srv = ThreadedServer(Echo(), port = PORTA, protocol_config = {"allow_public_attrs" : True})
 	srv.start()
 
 
